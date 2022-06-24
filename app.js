749aab09595b41f1fa49bac95992b86027e18042
Dyrits <dyrits@outlook.com>
2022-06-24 13:44:52 -0500

    Quotes modified.

--- app.js
+++ app.js
@@ -1,46 +1,46 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const session = require('express-session');
-const passport = require('passport');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const session = require("express-session");
+const passport = require("passport");
 
-const mongoose = require('./database/mongoose');
+const mongoose = require("./database/mongoose");
 
 
 const routers = {
-  index: require('./routes/index'),
-  users: require('./routes/users'),
-  posts: require('./routes/posts'),
-  reviews: require('./routes/reviews')
+  index: require("./routes/index"),
+  users: require("./routes/users"),
+  posts: require("./routes/posts"),
+  reviews: require("./routes/reviews")
 };
 
-const User = require('./models/user');
+const User = require("./models/user");
 
 const app = express();
 
 mongoose.connect();
-mongoose.database.on('error', console.error.bind(console, 'Connection error:'));
-mongoose.database.once('open', () => {
-    console.log('Connected to MongoDB!');
+mongoose.database.on("error", console.error.bind(console, "Connection error:"));
+mongoose.database.once("open", () => {
+    console.log("Connected to MongoDB!");
 });
 
 
 // View engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // Middlewares:
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Session configuration:
 app.use(session({
-    secret: '#SECRET#',
+    secret: "#SECRET#",
     resave: false,
     saveUninitialized: true,
 }));
@@ -53,10 +53,10 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 // Mount routes
-app.use('/', routers.index);
-app.use('/users', routers.users);
-app.use('/posts', routers.posts);
-app.use('/posts/:post/reviews', routers.reviews);
+app.use("/", routers.index);
+app.use("/users", routers.users);
+app.use("/posts", routers.posts);
+app.use("/posts/:post/reviews", routers.reviews);
 
 // Catch 404 and forward to error handler
 app.use((request, response, next) => {
@@ -67,10 +67,10 @@ app.use((request, response, next) => {
 app.use((error, request, response, next) => {
   // Set locals, only providing error in development
   response.locals.message = error.message;
-  response.locals.error = request.app.get('env') === 'development' ? error : {};
+  response.locals.error = request.app.get("env") === "development" ? error : {};
   // Render the error page
   response.status(error.status || 500);
-  response.render('error');
+  response.render("error");
 });
 
 module.exports = app;
