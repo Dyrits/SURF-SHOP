19d076a200ebc437178a3aaf53402c15076f42c2
Dylan J. Gerrits <dyrits@outlook.com>
2023-03-19 12:50:46 +0100

    Unnecessary key has been removed.

--- services/mapbox.js
+++ services/mapbox.js
@@ -4,7 +4,7 @@ const client = geocoding({ accessToken: process.env.MAPBOX_TOKEN });
 module.exports = {
   search: async location =>
     client
-      .forwardGeocode({ limit: 1, query: location, abc: "zefzef" })
+      .forwardGeocode({ limit: 1, query: location })
       .send()
       .then(response => response?.body?.features[0]?.geometry?.coordinates)
 };
